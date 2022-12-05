bbb6f925e7cf55653fbf3a534e0c3f928d34e2b9
jalimena <jalimena@users.noreply.github.com>
2022-12-05 12:37:33 +0000

    deploy: 99191196833ee8eaf3fea4584a72e7f6316f0667

--- doc/latest/search/all_12.js
+++ doc/latest/search/all_12.js
@@ -4,5 +4,6 @@ var searchData=
   ['test_5fcase_1',['TEST_CASE',['../onnxtest_8cpp.html#a6879f21f91348c4369df581a5834ff39',1,'onnxtest.cpp']]],
   ['test_5ffile_2',['TEST_FILE',['../onnxtest_8cpp.html#ad05e6c0cad6477bf659ce541e115012e',1,'onnxtest.cpp']]],
   ['thrustfit_3',['thrustFit',['../struct_f_c_c_analyses_1_1_algorithms_1_1thrust_fit.html',1,'FCCAnalyses::Algorithms::thrustFit'],['../struct_f_c_c_analyses_1_1_algorithms_1_1thrust_fit.html#a48de3d0fb8a8057e70b216fa4531b154',1,'FCCAnalyses::Algorithms::thrustFit::thrustFit()']]],
-  ['tokenise_4',['TOKENISE',['../onnxtest_8cpp.html#a7a904b81e786d4e88008709efe39b9a3',1,'onnxtest.cpp']]]
+  ['tokenise_4',['TOKENISE',['../onnxtest_8cpp.html#a7a904b81e786d4e88008709efe39b9a3',1,'onnxtest.cpp']]],
+  ['tracks_5',['tracks',['../struct_f_c_c_analyses_1_1_vertexing_utils_1_1_f_c_c_analyses_vertex.html#a30eac771013cfc920207b8fd902c4d59',1,'FCCAnalyses::VertexingUtils::FCCAnalysesVertex']]]
 ];
--- doc/latest/search/functions_b.js
+++ doc/latest/search/functions_b.js
@@ -4,7 +4,8 @@ var searchData=
   ['merge_1',['merge',['../namespace_f_c_c_analyses_1_1_reconstructed_particle.html#ab2d5b265b6346572a571bd2c38d8defe',1,'FCCAnalyses::ReconstructedParticle']]],
   ['merge_5fvertexobjet_2',['merge_VertexObjet',['../namespace_f_c_c_analyses_1_1my_utils.html#a38f36bd8b5b44dd8e6cad72dff073890',1,'FCCAnalyses::myUtils']]],
   ['mergeparticles_3',['mergeParticles',['../namespace_f_c_c_analyses_1_1_m_c_particle.html#ab1315bf243164c0a4fae1417fcf2b7fd',1,'FCCAnalyses::MCParticle']]],
-  ['minimize_5fsphericity_4',['minimize_sphericity',['../struct_f_c_c_analyses_1_1_algorithms_1_1minimize__sphericity.html#af1d63789d789a06799ad0f19e33ec559',1,'FCCAnalyses::Algorithms::minimize_sphericity']]],
-  ['minimize_5fthrust_5',['minimize_thrust',['../struct_f_c_c_analyses_1_1_algorithms_1_1minimize__thrust.html#a0cf23f7da89f793d34a07b3d12824685',1,'FCCAnalyses::Algorithms::minimize_thrust']]],
-  ['minus_6',['minus',['../struct_f_c_c_analyses_1_1_algorithms_1_1calculate__thrust.html#a787518d92bb7e99da7c12c6585802ebb',1,'FCCAnalyses::Algorithms::calculate_thrust']]]
+  ['mergevertices_4',['mergeVertices',['../namespace_f_c_c_analyses_1_1_vertexing_utils.html#aed1f9047902d1699311c1724b525dc0e',1,'FCCAnalyses::VertexingUtils']]],
+  ['minimize_5fsphericity_5',['minimize_sphericity',['../struct_f_c_c_analyses_1_1_algorithms_1_1minimize__sphericity.html#af1d63789d789a06799ad0f19e33ec559',1,'FCCAnalyses::Algorithms::minimize_sphericity']]],
+  ['minimize_5fthrust_6',['minimize_thrust',['../struct_f_c_c_analyses_1_1_algorithms_1_1minimize__thrust.html#a0cf23f7da89f793d34a07b3d12824685',1,'FCCAnalyses::Algorithms::minimize_thrust']]],
+  ['minus_7',['minus',['../struct_f_c_c_analyses_1_1_algorithms_1_1calculate__thrust.html#a787518d92bb7e99da7c12c6585802ebb',1,'FCCAnalyses::Algorithms::calculate_thrust']]]
 ];
