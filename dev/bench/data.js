c6d3face6d5b214e61bb4168c8e25696607fb826
github-action-benchmark <github@users.noreply.github.com>
2022-12-05 12:45:21 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for 99191196833ee8eaf3fea4584a72e7f6316f0667

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1670244006344,
+  "lastUpdate": 1670244320939,
   "repoUrl": "https://github.com/jalimena/FCCAnalyses",
   "entries": {
     "Benchmark": [
@@ -4872,6 +4872,58 @@ window.BENCHMARK_DATA = {
             "name": "nightly | Events processed per second: examples/FCCee/test",
             "value": 7.7166851986767195,
             "unit": "Evt/s"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "juliette.alimena@cern.ch",
+            "name": "Juliette Alimena",
+            "username": "jalimena"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "99191196833ee8eaf3fea4584a72e7f6316f0667",
+          "message": "Merge pull request #15 from mvoorde/add_exoticHiggsDecays_DVfinder_code\n\nAdd exotic higgs decays dv finder code",
+          "timestamp": "2022-12-02T17:08:47+01:00",
+          "tree_id": "555a95c5bfbc30f6def9b786d92ebb7328010dc1",
+          "url": "https://github.com/jalimena/FCCAnalyses/commit/99191196833ee8eaf3fea4584a72e7f6316f0667"
+        },
+        "date": 1670244318437,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "prod | Time spent running the analysis: case-studies/analysis_example/scripts",
+            "value": 13.031331300735474,
+            "unit": "Seconds",
+            "range": 10,
+            "extra": "Analysis path: case-studies/analysis_example/scripts"
+          },
+          {
+            "name": "prod | Time spent running the analysis: examples/FCCee/higgs/mH-recoil/mumu",
+            "value": 14.872111082077026,
+            "unit": "Seconds",
+            "range": 10,
+            "extra": "Analysis path: examples/FCCee/higgs/mH-recoil/mumu"
+          },
+          {
+            "name": "prod | Time spent running the analysis: examples/FCCee/flavour/Bc2TauNu",
+            "value": 20.09819746017456,
+            "unit": "Seconds",
+            "range": 10,
+            "extra": "Analysis path: examples/FCCee/flavour/Bc2TauNu"
+          },
+          {
+            "name": "prod | Time spent running the analysis: examples/FCCee/test",
+            "value": 15.79444432258606,
+            "unit": "Seconds",
+            "range": 10,
+            "extra": "Analysis path: examples/FCCee/test"
           }
         ]
       }
